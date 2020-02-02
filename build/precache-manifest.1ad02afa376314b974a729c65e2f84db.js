40d3392a6d593981757bc5b49aa0bd56aae49e79
leonard <lool4516@gmail.com>
2020-02-02 01:59:07 -0500

    build

---
+++ build/precache-manifest.1ad02afa376314b974a729c65e2f84db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c9258da85b8613332179622cd1c9cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4bb254f0875aa3ed09",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aac461e9759edf0048e4",
+    "url": "/static/js/2.4553c88a.chunk.js"
+  },
+  {
+    "revision": "cae37a785db540e0a9fa24b44a548f2c",
+    "url": "/static/js/2.4553c88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4bb254f0875aa3ed09",
+    "url": "/static/js/main.155b3c2a.chunk.js"
+  },
+  {
+    "revision": "78a4bb4a5d710a9aabf6",
+    "url": "/static/js/runtime-main.c310158f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
